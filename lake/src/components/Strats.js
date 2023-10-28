bde268cc227fcf57fb2ea4059d5d4da456dcd410
jk983294 <jk983294@gmail.com>
2023-10-28 10:24:56 +0800

    strats

--- lake/src/components/Strats.js
+++ lake/src/components/Strats.js
@@ -9,7 +9,19 @@ function rowClassNameFormat(row, rowIdx) {
 
 function percentFormatter(cell, row) {
    let val = ((Number.parseFloat(cell) * 100).toFixed(2)).toString() + '%'
-   return `${val}`;
+   let class_name = "td-Red-Cell";
+   let value = Number.parseFloat(cell);
+   if (value < 0) class_name = "td-Green-Cell";
+   if (Math.abs(value) < 1e-9) class_name = "";
+   return (<div class={class_name}>{val}</div>);
+}
+
+function dirFormatter(cell, row) {
+   let value = Number.parseFloat(cell);
+   let class_name = "";
+   if (value < -1e-9) class_name = "td-Green-Cell";
+   else if (value > 1e-9) class_name = "td-Red-Cell";
+   return (<div class={class_name}>{cell}</div>);
 }
 
 function mainPdtCellFormatter(cell, row) {
@@ -78,21 +90,24 @@ class Strats extends React.Component {
             <h1>strats page</h1>
             <BootstrapTable data={this.state.strats_data} striped
                options={{ noDataText: 'empty strats data' }}
-               trClassName={rowClassNameFormat} cellEdit={{ mode: 'dbclick', blurToSave: true }} version='4'>
+               // trClassName={rowClassNameFormat} 
+               cellEdit={{ mode: 'dbclick', blurToSave: true }} version='4'>
                <TableHeaderColumn isKey dataField='ins' dataFormat={mainPdtCellFormatter} dataSort={true}>Name</TableHeaderColumn>
                <TableHeaderColumn dataField='mins' dataFormat={pdtCellFormatter} >mins</TableHeaderColumn>
                <TableHeaderColumn dataField='start_obv_di' >so_di</TableHeaderColumn>
                <TableHeaderColumn dataField='end_obv_di' >eo_di</TableHeaderColumn>
-               <TableHeaderColumn dataField='hold_len' >hold_len</TableHeaderColumn>
+               <TableHeaderColumn dataField='hold_len' width="55" >len</TableHeaderColumn>
                <TableHeaderColumn dataField='day0' >day0</TableHeaderColumn>
                <TableHeaderColumn dataField='day1' >day1</TableHeaderColumn>
                <TableHeaderColumn dataField='obv_ret' dataFormat={percentFormatter} >obv_ret</TableHeaderColumn>
                <TableHeaderColumn dataField='max_drop_ratio' dataFormat={percentFormatter} >drop_ratio</TableHeaderColumn>
                <TableHeaderColumn dataField='ret' dataFormat={percentFormatter} >ret</TableHeaderColumn>
                <TableHeaderColumn dataField='ld_ret' dataFormat={percentFormatter} >ld_ret</TableHeaderColumn>
-               <TableHeaderColumn dataField='dir' >dir</TableHeaderColumn>
-               <TableHeaderColumn dataField='sid' >sid</TableHeaderColumn>
-               <TableHeaderColumn dataField='trend' >trend</TableHeaderColumn>
+               <TableHeaderColumn dataField='dir' width="55" dataFormat={dirFormatter} >dir</TableHeaderColumn>
+               <TableHeaderColumn dataField='sid' width="55" >sid</TableHeaderColumn>
+               <TableHeaderColumn dataField='trend' width="55" >trend</TableHeaderColumn>
+               <TableHeaderColumn dataField='c_oi5' dataFormat={percentFormatter} >oi5</TableHeaderColumn>
+               <TableHeaderColumn dataField='c_oi1' dataFormat={percentFormatter} >oi1</TableHeaderColumn>
             </BootstrapTable>
          </div>
       )
